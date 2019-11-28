0fcfdd7d33dc6a55b17e1b8e24a37e3bcf46bd47
tiforn <info@developerS2.com>
2019-11-28 21:40:00 +0200

    create project

---
+++ app/src/main/java/com/example/something/ChooseTheGasSation.java
@@ -0,0 +1,14 @@
+package com.example.something;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseTheGasSation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_the_gas_station);
+    }
+}
