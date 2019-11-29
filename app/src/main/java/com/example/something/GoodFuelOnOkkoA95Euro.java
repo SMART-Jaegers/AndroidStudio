ebfa6b98fa4da3c020a59f8c104a75807b6f1b7e
BigWave8 <rostykchess@gmail.com>
2019-11-29 09:16:54 +0200

    Adding good fuel screen

---
+++ app/src/main/java/com/example/something/GoodFuelOnOkkoA95Euro.java
@@ -0,0 +1,14 @@
+package com.example.something;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoodFuelOnOkkoA95Euro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_good_fuel_on_okko_a95_euro);
+    }
+}
