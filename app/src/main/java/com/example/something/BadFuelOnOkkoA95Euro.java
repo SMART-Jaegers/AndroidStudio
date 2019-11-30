1cd7aad04789ddbdda8e89b00fe46e06352f5b08
BigWave8 <rostykchess@gmail.com>
2019-11-30 14:32:34 +0200

    screen with bad fuel and 19 API

--- app/src/main/java/com/example/something/BadFuelOnOkkoA95Euro.java
+++ app/src/main/java/com/example/something/BadFuelOnOkkoA95Euro.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class GoodFuelOnOkkoA95Euro extends AppCompatActivity {
+public class BadFuelOnOkkoA95Euro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_good_fuel_on_okko_a95_euro);
+        setContentView(R.layout.activity_bad_fuel_on_okko_a95_euro);
     }
     public void backToOkko(View v) {
         switch (v.getId()) {
