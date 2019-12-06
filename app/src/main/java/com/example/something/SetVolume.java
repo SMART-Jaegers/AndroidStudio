552aaa0955c277573ef1fcff51c4f8fe540c4313
BigWave8 <rostykchess@gmail.com>
2019-12-06 14:00:45 +0200

    all layouts have 56 dp on top layout

--- app/src/main/java/com/example/something/SetVolume.java
+++ app/src/main/java/com/example/something/SetVolume.java
@@ -1,26 +1,36 @@
 package com.example.something;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.InputFilter;
 import android.text.InputType;
-import android.text.Spanned;
+import android.util.JsonReader;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SetVolume extends AppCompatActivity {
     EditText numberEditText;
+    String nothing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_volume2);
         numberEditText = (EditText) findViewById(R.id.editText);
         numberEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
     }
+
     public void backtomain(@NotNull View v) {
         switch (v.getId()) {
             case R.id.btnback:
@@ -31,19 +41,20 @@ public class SetVolume extends AppCompatActivity {
                 break;
         }
     }
+
     public void gotovolumecompare(@NotNull View v) {
         switch (v.getId()) {
             case R.id.btncheckfuel:
                 Intent intent = new Intent(this, VolumeCompare.class);
-                intent.putExtra("volume",numberEditText.getText().toString());
+                intent.putExtra("volume", numberEditText.getText().toString());
+                intent.putExtra("realvolume", nothing);
                 startActivity(intent);
                 break;
             default:
                 break;
         }
     }
+
 }
-
-
 
 
--- app/src/main/java/com/example/something/VolumeCompare.java
+++ app/src/main/java/com/example/something/VolumeCompare.java
@@ -8,20 +8,23 @@ import android.view.View;
 import android.widget.TextView;
 
 public class VolumeCompare extends AppCompatActivity {
-TextView textvolume;
+TextView textvolume, textresult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume_compare);
         textvolume = (TextView) findViewById(R.id.expectedvolume);
+        textresult = (TextView) findViewById(R.id.volumeResult);
         Intent intent = getIntent();
         String volume= intent.getStringExtra("volume");
+        String realvolume= intent.getStringExtra("realvolume");
+
         if (volume==null) {
             volume="0.0";
         }
-
         volume=volume + " liters";
         textvolume.setText(volume);
+        textresult.setText(realvolume + " liters");
     }
     public void backtomain(View v) {
         switch (v.getId()) {
