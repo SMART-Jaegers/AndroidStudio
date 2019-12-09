8fc8cc691eb1d810ca312c0763d0296456213d57
andrii.peleno <777andriypeleno777@gmail.com>
2019-12-09 19:14:27 +0200

    create connection to server

--- app/src/main/java/com/example/something/SetVolume.java
+++ app/src/main/java/com/example/something/SetVolume.java
@@ -4,30 +4,62 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.JsonReader;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class SetVolume extends AppCompatActivity {
     EditText numberEditText;
     String nothing;
+    TextView textRPi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_volume2);
         numberEditText = (EditText) findViewById(R.id.editText);
         numberEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        textRPi = (TextView)findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                AsyncTask asyncTask = new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        OkHttpClient client = new OkHttpClient();
+                        Request request = new Request.Builder()
+                                .url("http://192.168.43.87:5000")
+                                .build();
+
+                                Response response = null;
+                        try{
+                            response = client.newCall(request).execute();
+                            return response.body().string();
+
+                        } catch (IOException e){
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                    @Override
+                    protected void onPostExecute(Object o){
+                        textRPi.setText(o.toString());
+                    }
+                }.execute();
+            }
+        } );
 
     }
 
