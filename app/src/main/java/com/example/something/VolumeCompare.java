41420985284326c984b827dc9f6e20e5b89fe7d2
BigWave8 <rostykchess@gmail.com>
2019-12-10 16:31:25 +0200

    add screen with good volume compare

--- app/src/main/java/com/example/something/VolumeCompare.java
+++ app/src/main/java/com/example/something/VolumeCompare.java
@@ -1,18 +1,18 @@
 package com.example.something;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class VolumeCompare extends AppCompatActivity {
 TextView textvolume, textresult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_volume_compare);
+        setContentView(R.layout.activity_volume_compare_bad);
         textvolume = (TextView) findViewById(R.id.expectedvolume);
         textresult = (TextView) findViewById(R.id.volumeResult);
         Intent intent = getIntent();
---
+++ app/src/main/java/com/example/something/VolumeCompareGood.java
@@ -0,0 +1,14 @@
+package com.example.something;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VolumeCompareGood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volume_compare_good);
+    }
+}
