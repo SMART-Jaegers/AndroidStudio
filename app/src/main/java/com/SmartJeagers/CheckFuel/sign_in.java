2c4d41480d99e7b9d8ad154af79e61b62f30a2f3
BigWave8 <rostykchess@gmail.com>
2020-04-28 13:41:59 +0300

    Added user sign in layout and fixed warning

---
+++ app/src/main/java/com/SmartJeagers/CheckFuel/sign_in.java
@@ -0,0 +1,16 @@
+package com.SmartJeagers.CheckFuel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.checkfuel.something.R;
+
+public class sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
