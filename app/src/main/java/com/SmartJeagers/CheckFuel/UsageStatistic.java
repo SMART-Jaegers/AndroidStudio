0db4e03cf74e1dc4eb0a008de83db0218e03a254
tiforn <tarasfqx@gmail.com>
2020-05-03 20:49:57 +0300

    reformat usage statistic class and activities

--- app/src/main/java/com/SmartJeagers/CheckFuel/UsageStatistic.java
+++ app/src/main/java/com/SmartJeagers/CheckFuel/UsageStatistic.java
@@ -1,14 +1,11 @@
 package com.SmartJeagers.CheckFuel;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.checkfuel.something.R;
 
-public class usage_statistic extends AppCompatActivity {
-
-    @Override
+public class UsageStatistic extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usage_statistic);
