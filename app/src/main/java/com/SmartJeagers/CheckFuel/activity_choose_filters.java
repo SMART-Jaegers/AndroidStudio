aed0935302bd28a834ebda92977d37d7a3a82c67
Maxym_Zvarych <2811maximus@gmail.com>
2020-05-19 09:35:53 +0300

    Added lists and items for screen choose filters.

---
+++ app/src/main/java/com/SmartJeagers/CheckFuel/activity_choose_filters.java
@@ -0,0 +1,14 @@
+package com.SmartJeagers.CheckFuel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_choose_filters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_filters);
+    }
+}
